c5e8971c7f15d2d4ea6e51dd4828b006b734d45c
Jonathan Ato Markin <jona10711@gmail.com>
2022-03-15 14:47:35 +0000

    First commit

---
+++ src/main/java/com/ebg/dine/dine/DineApplication.java
@@ -0,0 +1,13 @@
+package com.ebg.dine.dine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebg/dine/dine/DineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebg.dine.dine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
